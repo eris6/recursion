fa02f712ae2616401540626ae601400d77a8977c
eris6 <erispatel20@gmail.com>
2024-12-31 16:58:16 -0500

    prettify

--- fibonacci.js
+++ fibonacci.js
@@ -1,23 +1,32 @@
-function fibs(n){
-    let fibArray = [];
-    fibArray.push(0);
-    fibArray.push(1);
+function fibs(n)
+{
+	let fibArray = [];
+	fibArray.push(0);
+	fibArray.push(1);
 
-    for (let i = 2; i < n; i++){
-        let sum = fibArray[i - 1] + fibArray[i - 2];
-        fibArray.push(sum);
-    }
-    return fibArray;
+	for (let i = 2; i < n; i++)
+	{
+		let sum = fibArray[i - 1] + fibArray[i - 2];
+		fibArray.push(sum);
+	}
+	return fibArray;
 }
 
-function fibsRec(n){
-    if (n == 1){return [0]}
-    else if (n == 2){return [0,1]}
+function fibsRec(n)
+{
+	if (n == 1)
+	{
+		return [0]
+	}
+	else if (n == 2)
+	{
+		return [0, 1]
+	}
 
-    const arr = fibsRec(n - 1);
-    arr.push(arr[arr.length - 1] + arr[arr.length - 2])
+	const arr = fibsRec(n - 1);
+	arr.push(arr[arr.length - 1] + arr[arr.length - 2])
 
-    return arr;
+	return arr;
 }
 
 
--- mergesort.js
+++ mergesort.js
@@ -1,32 +1,38 @@
-function mergeSort(arr){
-    if (arr.length === 1){
-        return arr;
-    }
+function mergeSort(arr)
+{
+	if (arr.length === 1)
+	{
+		return arr;
+	}
 
-    let leftHalf = arr.slice(0, arr.length/2);
-    let rightHalf = arr.slice(arr.length/2, arr.length);
+	let leftHalf = arr.slice(0, arr.length / 2);
+	let rightHalf = arr.slice(arr.length / 2, arr.length);
 
-    return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+	return merge(mergeSort(leftHalf), mergeSort(rightHalf));
 
 }
 
-function merge(left, right){
-    const result = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
+function merge(left, right)
+{
+	const result = [];
+	let leftIndex = 0;
+	let rightIndex = 0;
 
 
-    while (leftIndex < left.length && rightIndex < right.length){
-        if (left[leftIndex] < right[rightIndex]){
-            result.push(left[leftIndex]);
-            leftIndex++;
-        }
-        else{
-            result.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+	while (leftIndex < left.length && rightIndex < right.length)
+	{
+		if (left[leftIndex] < right[rightIndex])
+		{
+			result.push(left[leftIndex]);
+			leftIndex++;
+		}
+		else
+		{
+			result.push(right[rightIndex]);
+			rightIndex++;
+		}
+	}
+	return result.concat(left.slice(leftIndex), right.slice(rightIndex));
 }
 
-console.log(mergeSort([4,3,10,22, 1, 5]))
+console.log(mergeSort([4, 3, 10, 22, 1, 5]))
