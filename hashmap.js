5f8cb80ce0921ce434468d004deccabfc7630b6f
Eris <erisittera@gmail.com>
2025-08-17 09:45:42 -0400

    Write keys, values, and entries functions

--- hashmap.js
+++ hashmap.js
@@ -95,19 +95,77 @@ function HashMap(){
 
     const clear = () => {
         for (let i = 0; i < capacity; i++){
-            buckets[i] = []
+            buckets[i] = [];
         }
     }
 
+    const keys = () => {
+        const keyList = [];
 
-    return {set, get, has, remove, length, clear}
+        for (let i = 0; i < capacity; i++){
+            let currentBucket = buckets[i];
+
+            currentBucket.forEach((item) => {
+                keyList.push(item[0]);
+            })
+        }
+
+        return keyList;
+    }
+
+
+    const values = () => {
+        const keyList = [];
+
+        for (let i = 0; i < capacity; i++){
+            let currentBucket = buckets[i];
+
+            currentBucket.forEach((item) => {
+                keyList.push(item[1]);
+            })
+        }
+
+        return keyList;
+    }
+
+        const entries = () => {
+        const keyList = [];
+
+        for (let i = 0; i < capacity; i++){
+            let currentBucket = buckets[i];
+
+            currentBucket.forEach((item) => {
+                keyList.push([item[0], item[1]]);
+            })
+        }
+
+        return keyList;
+    }
+    return {set, get, has, remove, length, clear, keys, values, entries}
 
 }
 
 const test = new HashMap();
-test.set('carrot', 'orange');
-test.set('butterfly', 'orange');
-test.clear();
+ test.set('apple', 'red')
+ test.set('banana', 'yellow')
+ test.set('carrot', 'orange')
+ test.set('dog', 'brown')
+ test.set('elephant', 'gray')
+ test.set('frog', 'green')
+ test.set('grape', 'purple')
+ test.set('hat', 'black')
+ test.set('ice cream', 'white')
+ test.set('jacket', 'blue')
+ test.set('kite', 'pink')
+ test.set('butterfly', 'blue')
+ test.set('lion', 'golden')
+  test.set('moon', 'silver')
+
+
+  test.get('banana')
+
+console.log(test.keys());
+console.log(test.entries());
 
 
 
