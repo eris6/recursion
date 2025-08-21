275ca201fc7529c2f33a0d9472b50e868fff502c
Eris <erisittera@gmail.com>
2025-08-21 19:36:48 -0400

    Bfs, dfs, isbalanced, and reBalance functions

--- bst.js
+++ bst.js
@@ -12,7 +12,6 @@ const Tree = (array) => {
         if (start > end) return null;
 
         let mid = start + Math.floor((end - start) / 2);
-
         let node = Node(array[mid]);
 
         node.left = buildTreeRecursive(array, start, mid - 1);
@@ -131,19 +130,197 @@ const Tree = (array) => {
         return findRecurse(root, value);
     }
 
+    const logValueCallback = (node) => {
+    console.log(node.data);
+    };
+
+
+    const levelOrderIterative = (node, callback) => {
+        if (typeof callback !== 'function'){
+            throw new Error('Not a function!');
+        }
+
+        if (node === null){
+            return [];
+        }
+
+        const queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            const currentNode = queue.shift();
+            callback(currentNode);
+
+            if (currentNode.left){
+            queue.push(currentNode.left);
+            }
+
+            if (currentNode.right){
+                queue.push(currentNode.right);
+            }
+        }
+    }
+
+    const levelOrderForEach = (callback) => {
+        levelOrderIterative(root, callback);
+    }
+
+    const inOrderRecursive = (node, callback) => {
+        if (node === null){
+            return;
+        }
+
+        inOrderRecursive(node.left, callback);
+        callback(node);
+        inOrderRecursive(node.right, callback);
+
+    }
+
+    const inOrderForEach = (callback) => {
+        inOrderRecursive(root, callback);
+    }
+
+    const preOrderRecursive = (node, callback) => {
+        if (node === null){
+            return;
+        }
+
+        callback(node);
+        preOrderRecursive(node.left, callback);
+        preOrderRecursive(node.right, callback);
+    }
+
+    const preOrderForEach = (callback) => {
+        preOrderRecursive(root, callback);
+    }
+
+    const postOrderRecursive = (node, callback) => {
+        if (node === null){
+            return;
+        }
+        postOrderRecursive(node.left, callback);
+        postOrderRecursive(node.right, callback);
+        callback(node);
+    }
+
+    const postOrderForEach = (callback) => {
+        postOrderRecursive(root, callback);
+    }
+
+    const heightRecurse = (node) => {
+        if (node === null){
+            return 0;
+        }
+
+        const left = heightRecurse(node.left);
+        const right = heightRecurse(node.right);
+
+        return 1 + Math.max(left, right);
+    }
+
+
+    const height = (value) => {
+        const givenNode = find(value);
+        return heightRecurse(givenNode);
+    }
+
+    const depthRecurse = (node, value, currentDepth = 0) => {
+        if (node === null){
+            return -1;
+        }
+
+        if (node.data === value){
+            return currentDepth;
+        }
+
+        if (value < node.data){
+            return depthRecurse(node.left, value, currentDepth + 1);
+        }
+
+        else{
+            return depthRecurse(node.right, value, currentDepth + 1);
+        }
+
+    } 
+
+    const depth = (value) => {
+        return depthRecurse(root, value);
+    }
+
+    const checkHeightAndBalance = (node) => {
+        if (node === null){
+            return 0;
+        }
+
+        const leftHeight = checkHeightAndBalance(node.left);
+        if (leftHeight === -1){
+            return -1;
+        }
+
+        const rightHeight = checkHeightAndBalance(node.right);
+        if (rightHeight === -1){
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return (1 + Math.max(leftHeight, rightHeight))
+    }
+
+
+    const isBalanced = () => {
+        return checkHeightAndBalance(root) !== -1;
+    }
+
+    const rebalanceRecurse = (node, res= []) => {
+        if (node === null){
+            return;
+        }
+
+        res.push(node.data);
+        rebalanceRecurse(node.left, res);
+        rebalanceRecurse(node.right, res);
+
+        return res;
+
+    }
+
+
+    const rebalance = () => {
+        const newNodes = rebalanceRecurse(root);
+        console.log(newNodes);
+        root = buildTree(newNodes);
+    }
+
+
 
 
 
 
     prettyPrint(root);
 
-    return {insert, deleteItem, find}
+    return {insert, deleteItem, find, levelOrderForEach, logValueCallback, inOrderForEach, preOrderForEach, postOrderForEach, 
+        height, depth, isBalanced, rebalance
+    }
 
 }
 
 let tree1 = Tree([34, 54, 65, 90, 100, 201, 302]);
 
 
-console.log(tree1.find(90000))
+console.log(tree1.isBalanced());
+tree1.insert(305)
+tree1.insert(306)
+tree1.insert(307)
+tree1.insert(308)
+tree1.insert(309)
+console.log(tree1.isBalanced());
+
+console.log(tree1.rebalance())
+tree1.insert(310)
+
+
 
 
